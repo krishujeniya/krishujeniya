ca76d7efd3d04dc07f670cb1ef467997ef6e74fc
Krish Ujeniya <100012208+krishujeniya@users.noreply.github.com>
2025-05-05 13:55:36 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,30 @@
+import type {NextConfig} from 'next';
+
+// Assuming the repository name is 'Krish-Ujeniya-Portfolio' for GitHub Pages deployment
+const repoName = 'krishujeniya21';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export', // Export as static site
+  basePath: process.env.NODE_ENV === 'production' ? `/${repoName}` : '', // Set base path for GitHub Pages
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '', // Set asset prefix for GitHub Pages
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+   images: {
+     unoptimized: true, // Disable Next.js Image Optimization for static export compatibility
+     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
